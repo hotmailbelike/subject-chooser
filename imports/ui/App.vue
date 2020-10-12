4b05d8e4045c554f1427bd7c21cb618b56c13dc7
Abdullah Omar <fishilike.fish@gmail.com>
2020-10-13 03:26:38 +0600

    cleanup

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <hello/>
-    <info/>
-  </div>
+	<div>
+		<h1>Welcome to Meteor!</h1>
+		<Hello />
+	</div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
-import Info from './components/Info.vue'
+import Hello from './components/Hello.vue';
 
 export default {
-  components: {
-    Hello,
-    Info,
-  },
-}
+	components: {
+		Hello,
+	},
+};
 </script>
 
 <style>
-  body {
-    font-family: sans-serif;
-    padding: 10px;
-  }
+body {
+	font-family: sans-serif;
+	padding: 10px;
+}
 </style>
